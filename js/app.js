5d75dae42388ae2677f16ca2871bb34d033bdb8b
Masum <007khanmasum@gmail.com>
2022-08-15 18:53:55 +0600

    basic js added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+const listContainer=document.getElementById("listConatienr");
+const inputFeild= document.getElementById("todoInput");
+const todoSubmit= document.getElementById("todoSubmit");
+
+
+
+// rest of thing you have to do, after watching todys vedios
