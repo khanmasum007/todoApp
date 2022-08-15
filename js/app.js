afdd285917caf22575079b0b6ff1a206be420e71
Masum <007khanmasum@gmail.com>
2022-08-15 19:05:33 +0600

    3 line code done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ const listContainer=document.getElementById("listConatienr");
 const inputFeild= document.getElementById("todoInput");
 const todoSubmit= document.getElementById("todoSubmit");
 
+todoSubmit.addEventListener(
+   'click', function(){
+    
+   }
+)
 
 
 // rest of thing you have to do, after watching todys vedios
